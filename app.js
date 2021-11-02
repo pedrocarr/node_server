dfaf85eb761fd5050559dc1b5ff2b0966daa0898
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-02 20:31:58 -0300

    finish server

--- app.js
+++ app.js
@@ -3,10 +3,38 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
-        
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My Node Page</title><head>');
+        res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></body>');
+        res.write('</html>');
+        return res.end();
+       }
+    if (url === '/users') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My Node Page</title><head>');
+        res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+        res.write('</html>');
+        return res.end();
     }
 
+    // send HTML response with some "Page Not Found text"
+    if (url === '/create-user' && method === "POST") {
+        const body = [];
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody.split('='));
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+    }
 })
 
 
